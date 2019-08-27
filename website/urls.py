0ddada8fe7c0bd644b9d3232b206c5e78ef159f4
Taloi Stefan <taloistefanel8@gmail.com>
2019-08-27 13:07:51 +0300

    Settings

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('informatii/', views.informations, name='informatii'),
     path('regulament/', views.rules, name='regulament'),
     path('cereri-staff/', views.staffRequests, name='staffRequests'),
-    path('404/', views.error404, name='eroare'),
     path('cereri-staff/', views.sendmail, name='sendmail'),
+    path('404/', views.handler404, name='handler404'),
+    path('500/', views.handler500,  name='handler500')
 ]
