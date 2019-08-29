43d4cc41bac1eb0b06e8cdb698420283b7a47093
Taloi Stefan <taloistefanel8@gmail.com>
2019-08-29 15:28:50 +0300

    Settings

--- GrassCraft/settings.py
+++ GrassCraft/settings.py
@@ -30,7 +30,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'grass-craft'
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = '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'
 
 ALLOWED_HOSTS = ["*"]
