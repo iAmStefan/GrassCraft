0df3a29f937295a5a3119a98bb7ca5d5c272783a
Taloi Stefan <taloistefanel8@gmail.com>
2019-08-29 16:46:28 +0300

    Modifications

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('informatii/', views.informations, name='informatii'),
     path('regulament/', views.rules, name='regulament'),
-    path('cereri-staff/', views.staffRequests, name='staffRequests'),
-    path('sendmail', views.sendmail, name='sendmail'),
+    path('medele-cereri/', views.staffRequests, name='staffRequests'),
     path('404/', views.error404, name='error'),
 ]
